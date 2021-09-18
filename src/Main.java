16e771e1aa25320290f243037ac184a211d3d5b4
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-09-18 11:47:17 +0530

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+/*
+ *  * ------------------------------------------------------------------------------------------------------------------
+ *  *  *  Copyright (c) GIT-Testing. All rights reserved.
+ *  *  *  Licensed under the SriLankan Information License. See License.txt in the project root for license information.
+ *  * ------------------------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @author Mindula Dilthushan <minduladilthushan1@gmail.com>
+ * @since 9/18/2021
+ **/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Mindula");
